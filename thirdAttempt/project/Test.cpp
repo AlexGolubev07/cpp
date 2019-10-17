07d0fa912c47bc6d23afc3208bb72f25270d4dcf
AlexGolubev07 <55049595+AlexGolubev07@users.noreply.github.com>
2019-10-17 18:51:12 +0300

    Update Test.cpp

--- thirdAttempt/project/Test.cpp
+++ thirdAttempt/project/Test.cpp
@@ -1,18 +1,35 @@
-#include "gtest/gtest.h"
-#include "myMath.h"
-TEST(TestCaseName, TestName)
-{
-  EXPECT_EQ(myMath::extremum::max(1, 2), 2);
-  EXPECT_TRUE(true);
-  EXPECT_EQ(myMath::extremum::min(1, 2), 1);
-  EXPECP_TRUE(true);
-  EXPECT_EQ(myMath::extremum::maxFrom3Alt(1, 2, 3), 3);
-  EXPECT_TRUE(true);
-  EXPECT_EQ(myMath::extremum::max3(1, 2, 3), 3);
-  EXPECT_TRUE(true)
-  EXPECT_EQ(myMath::tWF::factorial(4), 24)  
-  EXPECT_EQ(myMath::tWF::facRec(4), 24)
-  EXPECT_EQ(myMath::tWF::factorialSum(4), 33)
-  EXPECT_EQ(myMath::tWF::factorialSumAlt(4), 33)
-	  EXPECT_EQ(myMath::tWF::)
+#include "pch.h"
+#include "../project/integerNumbers.h"
+
+#pragma region intNumGCD
+
+TEST(intNumGCD, GCD) {
+	EXPECT_EQ(intNum::gcd::gcd(10, 5), 5);
+	EXPECT_EQ(intNum::gcd::gcd(15, 15), 15);
 }
+
+TEST(intNumGCD, euclidRec) {
+	EXPECT_EQ(intNum::gcd::euclidRec(10, 5), 5);
+	EXPECT_EQ(intNum::gcd::euclidRec(15, 15), 15);
+}
+
+TEST(intNumGCD, euclidNotRec) {
+	EXPECT_EQ(intNum::gcd::euclidNotRec(10, 5), 5);
+	EXPECT_EQ(intNum::gcd::euclidNotRec(15, 15), 15);
+}
+
+#pragma endregion
+
+#pragma region intNum
+
+TEST(intNum, sumOfDigit) {
+	EXPECT_EQ(intNum::sumOfDigit(105), 6);
+	EXPECT_EQ(intNum::sumOfDigit(1001), 2);
+}
+
+TEST(intNum, lenNumber) {
+	EXPECT_EQ(intNum::lenNumber(105), 3);
+	EXPECT_EQ(intNum::lenNumber(1001), 4);
+}
+
+#pragma endregion 
