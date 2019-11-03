bd904ceef7fbd565d5c3bfaca2e4350e2e0ff801
AlexGolubev07 <55049595+AlexGolubev07@users.noreply.github.com>
2019-11-03 20:02:15 +0300

    ege 252 near the end

--- oh/Project1/ege251.cpp
+++ oh/Project1/ege251.cpp
@@ -283,10 +283,205 @@ namespace ege
 						min = a[i];
 					}
 				}
-				sred1 = (min + max) / 2;
+				sred1 = (min + max) / 2.0;
 				sred2 = sum / N;
 				min = sred1 - sred2;
 				cout << min;
+			}
+
+			void ege2925()
+			{
+				#define N 30
+				int a[N];
+				int i, x, y;
+				float s;
+				x = 0;
+				y = 0;
+				for (i = 0; i < N; i++)
+				{
+					cin >> a[i];
+				}
+				for (i = 0; i < N - 1; ++i)
+				{
+					if (a[i] < a[N])
+					{
+						x += a[i];
+						++y;
+					}
+				}
+				cout << x / double(y);
+			}
+
+			void ege3622()
+			{
+				#define N 40 
+				
+				int a[N]; 
+				int i, l, lmax, s, smax;
+				for (i = 0; i < N; i++)
+				{
+					cin >> a[i];
+				}
+				lmax = 0;
+				smax = 0;
+				s = 0;
+				for (i = 0; i < N - 1; ++i)
+				{
+					if (a[i] < a[i + 1])
+					{
+						++smax;
+					}
+					else
+					{
+						++smax;
+						if (smax > lmax)
+						{
+							lmax = smax;
+							l = i;
+						}
+						smax = 0;
+					}
+				}
+				for (i = 0; i < lmax - 1; ++i)
+				{
+					s += a[l - i];
+				}
+				cout << s;
+			}
+
+			void ege5289()
+			{
+				#define N 30 
+				
+				int a[N];
+				int i, j, s;
+				for (i = 0; i < N; i++)
+				{
+					cin >> a[i];
+				}
+				s = 0;
+				for (i = 0; i < N; ++i)
+				{
+					if (a[i] > 9 && a[i] < 100 && ((a[i] - (a[i] % 10)) / 10) > (a[i] % 10))
+					{
+						s += a[i];
+					}
+				}
+				cout << s;
+			}
+
+			void ege7383()
+			{
+				#define N 30
+				
+				int a[N];
+				int i, x, y;
+				float s;
+				for (i = 0; i < N; i++)
+				{
+					cin >> a[i];
+				}
+				x = 0;
+				y = 0;
+				for (i = 0; i < N; ++i)
+				{
+					if (a[i] == 4 || a[i] == 5)
+					{
+						x += a[i];
+						++y;
+					}
+				}
+				cout << x / double(y);
+			}
+
+			void ege6200()
+			{
+				#define N 2014 
+				
+				int a[N];
+				int i, j, s;
+				for (i = 0; i < N; i++)
+				{
+					cin >> a[i];
+				}
+				s = 0;
+				j = 0;
+				for (i = 0; i < N; ++i)
+				{
+					if (a[i] > 99 && a[i] < 1000 && a[i] % 10 == 9 && a[i] % 100 != 99)
+					{
+						s += a[i];
+						j = 1;
+					}
+				}
+				if (j != 0)
+				{
+					cout << s;
+				}
+				else
+				{
+					cout << -1;
+				}
+			}
+
+			void ege10301()
+			{
+				#define N 2016
+				
+				int a[N];
+				int i, k;
+				for (i = 0; i < N; i++)
+				{
+					cin >> a[i];
+				}
+				k = 0;
+				for (i = 0; i < N - 2; ++i)
+				{
+					if ((a[i + 1] + a[i + 2]) / 2 == a[i])
+					{
+						++k;
+					}
+				}
+				cout << k;
+			}
+
+			void ege10399()
+			{
+				#define N 2016
+				
+				int a[N];
+				int i, m, s, p;
+				for (i = 0; i < N; ++i)
+				{
+					cin >> a[i];
+				}
+				s = a[N];
+				if (a[N] % 2 == 0)
+				{
+					for (i = 0; i < N - 1; ++i)
+					{
+						if (a[i] % 2 == 0)
+						{
+							s += a[i];
+						}
+					}
+				}
+				else
+				{
+					for (i = 0; i < N - 1; ++i)
+					{
+						if (a[i] % 2 == 1)
+						{
+							s += a[i];
+						}
+					}
+				}
+				cout << s;
+			}
+
+			void ege13636()
+			{
+				// I need to know shestnadzatirichnost' well
 			}
 		}
 	}
