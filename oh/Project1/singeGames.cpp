83cae2bf24736a0a7efd5e52b356dcade47d06cd
AlexGolubev07 <55049595+AlexGolubev07@users.noreply.github.com>
2020-02-20 18:38:55 +0300

    zmejka

--- oh/Project1/singeGames.cpp
+++ oh/Project1/singeGames.cpp
@@ -2090,4 +2090,23 @@ namespace games
 		}
 		return 0;
 	}
+
+	int snake()
+	{
+	
+	}
+
+	int tetris()
+	{
+		int a[20][10];
+		for (int i = 0; i < 20; ++i)
+		{
+			for (int j = 0; j < 10; ++j)
+			{
+				a[i][j] = 0;
+				cout << a[i][j] << " ";
+			}
+			cout << endl;
+		}
+	}
 }
